c0515732e705701251b3505c8e87d05670fbea05
linjinbao666 <linjinbao666@gmail.com>
2021-04-15 22:54:42 +0800

    init

---
+++ assets/js/main.js
@@ -0,0 +1,70 @@
+﻿jQuery(function($) {
+
+    'use strict';
+
+    var _Blog = window._Blog || {};
+
+    _Blog.prettify = function() {
+        $('pre').addClass('prettyprint linenums').attr('style', 'overflow:auto;');
+        window.prettyPrint && prettyPrint();
+    };
+
+    _Blog.externalUrl = function() {
+        $.expr[':'].external = function(obj) {
+            return !obj.href.match(/^mailto\:/) &&
+                (obj.hostname != location.hostname);
+        };
+        $('a:external').addClass('external');
+        $(".external").attr('target', '_blank');
+
+    }
+
+    _Blog.changeTitle = function() {
+        var currentTitle = document.title;
+        window.onblur = function() {
+            document.title = 'linjinbao';
+        }
+        window.onfocus = function() {
+            document.title = currentTitle;
+        }
+    };
+
+    _Blog.toggleTheme = function() {
+        const currentTheme = window.localStorage && window.localStorage.getItem('theme')
+        const isDark = currentTheme === 'dark'
+        $('body').toggleClass('dark-theme', isDark)
+        $('.theme-switch').on('click', () => {
+            const iframe = document.querySelector('.utterances-frame');
+            $('body').toggleClass('dark-theme')
+            window.localStorage &&
+                window.localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark' : 'light', )
+            const dark_message = {
+                type: 'set-theme',
+                theme: 'github-dark'
+            };
+            const light_message = {
+                type: 'set-theme',
+                theme: 'github-light'
+            };
+            if(document.body.classList.contains('dark-theme')){
+                iframe.contentWindow.postMessage(dark_message, 'https://utteranc.es');
+            }else{
+                iframe.contentWindow.postMessage(light_message, 'https://utteranc.es');
+            }
+        })
+    }
+
+    _Blog.toggleMobileMenu = function() {
+        $('.menu-toggle').on('click', () => {
+            $('.menu-toggle').toggleClass('active')
+            $('#mobile-menu').toggleClass('active')
+        })
+    }
+
+    $(document).ready(function() {
+        _Blog.prettify()
+        _Blog.changeTitle()
+        _Blog.toggleTheme()
+        _Blog.toggleMobileMenu()
+    });
+});
